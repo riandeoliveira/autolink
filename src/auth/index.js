190b3f7bc0bfd22f1b1b2bd20020e65fb0824999
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-08-25 16:23:47 -0300

    refactor: move authenticateUser to auth index file

---
+++ src/auth/index.js
@@ -0,0 +1,18 @@
+const { input } = require("../data");
+const { EMAIL, PASSWORD } = require("../libs/dotenv");
+
+const authenticateUser = async (page) => {
+  // Fill email field.
+  await page.waitForSelector(input.email);
+  await page.type(input.email, EMAIL);
+
+  // Fill password field.
+  await page.waitForSelector(input.password);
+  await page.type(input.password, PASSWORD);
+
+  // Click submit button.
+  await page.waitForSelector(input.button);
+  await page.click(input.button);
+};
+
+module.exports = authenticateUser;
