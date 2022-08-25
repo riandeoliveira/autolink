349ec7220349656e414c8dbb21e935b585442aae
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-08-25 16:24:07 -0300

    refactor: change operator

--- src/libs/dotenv.js
+++ src/libs/dotenv.js
@@ -2,7 +2,7 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-const EMAIL = process.env.EMAIL || "";
-const PASSWORD = process.env.PASSWORD || "";
+const EMAIL = process.env.EMAIL ?? "";
+const PASSWORD = process.env.PASSWORD ?? "";
 
 module.exports = { EMAIL, PASSWORD };
